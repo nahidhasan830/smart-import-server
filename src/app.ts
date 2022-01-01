7714e4f6be875e1e8e93a8e18b3dcb58878abd72
nahidhasan830 <nahidhasan830@gmail.com>
2022-01-01 16:19:24 +0600

    fixed cors issue

--- src/app.ts
+++ src/app.ts
@@ -15,18 +15,12 @@ const app = express();
 app.use(compression());
 
 //CORS SETTINGS
-const whitelist = ['http://localhost:3000', 'https://smart-import.vercel.app'];
-const corsOptions = {
-  origin: function (origin: any, callback: any) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
 
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'https://smart-import.vercel.app']
+  })
+);
 
 app.use(helmet());
 
